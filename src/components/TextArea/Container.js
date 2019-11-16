d685d1639c8694db1d042c4d912620c40f8c87b7
jhyang12345 <jhyang12345@naver.com>
2019-11-16 17:40:26 +0900

    Trying to add resize handler

--- src/components/TextArea/Container.js
+++ src/components/TextArea/Container.js
@@ -5,13 +5,33 @@ import styled from 'styled-components'
 
 class ContainerComponent extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.containerRef = React.createRef()
+    }
+
+    componentDidMount = () => {
+        window.addEventListener('resize', this.handleContainerResize)
+    }
+
+    handleContainerResize = () => {
+        const curElem = this.containerRef.current
+        const width = curElem.offsetWidth;
+        console.log(width)
+    }
+
     render() {
         return (
-            <Container>
+            <Container
+                ref={this.containerRef}>
                 <Content>Text</Content>
                 <Content>Another test</Content>
             </Container>
         )
+    }
+
+    componentWillUnmount = () => {
+        window.removeEventListener('resize', this.handleContainerResize)
     }
 }
 
