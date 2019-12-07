b357c47c9db7d7e11861c6be7f48d64597a3a10f
jhyang12345 <jhyang12345@naver.com>
2019-12-07 23:28:39 +0900

    Attempting to implement rdsToHiveSelect

---
+++ src/components/RDSToHive/RDSToHiveSelectComponent.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import Common from '../Common'
+import TextArea from '../TextArea'
+import styled from 'styled-components'
+
+import { connect } from 'react-redux'
+
+class RDSToHiveSelectComponent extends React.Component {
+
+    runButtonHandler = () => {
+        const { dispatch, fromText } = this.props
+
+        console.log("Button Clicked")
+        dispatch(handleRdsToHiveCreate(fromText))
+    }
+
+    handleFromTextChange = (evt) => {
+        const { dispatch } = this.props
+        const text = evt.target.value
+        dispatch(handleSetFromText(text))
+    }
+
+    handleToTextChange = (evt) => {
+        const { dispatch } = this.props
+        const text = evt.target.value
+        dispatch(handleSetToText(text))
+    }
+
+    render() {
+        const {
+            fromText,
+            toText,
+            pending,
+            succeeded,
+            updating,
+        } = this.props
+
+        return (
+            <PageContainer>
+                <TextArea.Container
+                    height={'1200px'}
+                    flexDirection={'column'}>
+                    <TextArea.Component
+                        placeholder={"CREATE TABLE RDS..."}
+                        value={fromText}
+                        onChange={this.handleFromTextChange}
+                    >
+                    </TextArea.Component>
+                    <RunButton
+                        onClick={this.runButtonHandler}
+                    >
+                        RUN
+                    </RunButton>
+                    <TextArea.Component
+                        placeholder={"SELECT SCRIPT..."}
+                        value={toText}
+                        onChange={this.handleToTextChange}
+                        pending={pending}
+                        succeeded={succeeded}
+                        updating={updating}
+                        enableCopy={true}
+                    >
+                    </TextArea.Component>
+                </TextArea.Container>
+            </PageContainer>
+        )
+    }
+}
+
+const PageContainer = styled.div`
+  margin: 0px auto;
+  max-width: 1080px;
+`
+
+const RunButton = styled.div`
+  display: inline-block;
+  align-self: center;
+  padding: 6px 24px;
+  height: 42px;
+  line-height: 30px;
+  background-color: #326fad;
+  border-radius: 12px;
+  font-weight: bold;
+  font-size: 20px;
+  color: #FFF;
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const mapStateToProps = ({transformRDSToHiveCreate}) => {
+    const {
+        fromText,
+        toText,
+        pending,
+        succeeded,
+        updating
+    } = transformRDSToHiveCreate
+    return {
+        fromText,
+        toText,
+        pending,
+        succeeded,
+        updating,
+    }
+}
+
+export default connect(mapStateToProps)(RDSToHiveCreateComponent)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers} from "redux"
 import rdsToHiveCreate from "./RdsToHiveCreate";
+import rdsToHiveSelect from "./RDSToHiveSelect"
 import transformCreateToWiki from "./WikiTable";
 
 export default combineReducers({
+    transformRDSToHiveSelect: rdsToHiveSelect,
     transformRDSToHiveCreate: rdsToHiveCreate,
     transformCreateToWiki,
 })
